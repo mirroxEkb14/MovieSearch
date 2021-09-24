2bb690bfe3d77da3eb2160d07bc2cf7575bfd669
Daniyar <dancho.rancho07@gmail.com>
2021-09-24 17:58:57 +0200

    added film posters, buttons, toasts, link to website

--- app/src/main/java/com/amirovdev/moviesearch/MainActivity.kt
+++ app/src/main/java/com/amirovdev/moviesearch/MainActivity.kt
@@ -2,10 +2,49 @@ package com.amirovdev.moviesearch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    // buttons from 'main_activity'
+    private lateinit var btnMenu: Button
+    private lateinit var btnFavorites: Button
+    private lateinit var btnSeeLater: Button
+    private lateinit var btnSelections: Button
+    private lateinit var btnSettings: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initView()
+
+        btnMenu.setOnClickListener {
+            Toast.makeText(this, "Menu!", Toast.LENGTH_SHORT).show()
+        }
+
+        btnFavorites.setOnClickListener {
+            Toast.makeText(this, "Favorites!", Toast.LENGTH_SHORT).show()
+        }
+
+        btnSeeLater.setOnClickListener {
+            Toast.makeText(this, "See Later!", Toast.LENGTH_SHORT).show()
+        }
+        btnSelections.setOnClickListener {
+            Toast.makeText(this, "Selections!", Toast.LENGTH_SHORT).show()
+        }
+
+        btnSettings.setOnClickListener {
+            Toast.makeText(this, "Settings!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun initView() {
+        btnMenu = findViewById(R.id.btn_menu)
+        btnFavorites = findViewById(R.id.btn_favorites)
+        btnSeeLater = findViewById(R.id.btn_see_later)
+        btnSelections = findViewById(R.id.btn_selections)
+        btnSettings = findViewById(R.id.btn_settings)
     }
 }
